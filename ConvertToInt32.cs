d8a6a5ef08466fc81feac3924d73602ce75ac582
Ragini340 <ragini31bxr@gmail.com>
2022-12-01 08:17:38 +0530

    Add project files.

---
+++ ConvertToInt32.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertToInt32Method
+{
+    class ConvertToInt32
+    {
+     public static void Main(string[] args)
+        {
+            double number = 40.5;
+          
+            int num = Convert.ToInt32(number);
+          
+            Console.WriteLine("Converted number {0} to {1} in 32-bit signed integer.", number, num);
+            
+        }
+    }
+}
---
+++ ConvertToInt32POC.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertToInt32Method
+{
+    class ConvertToInt32POC
+    {
+        public static void Main(String[] args)
+        {
+            string[] values = { "One", "1.34e28", "-26.87", "-18", "-6.00",
+                    " 0", "137", "1601.9", Int32.MaxValue.ToString() };
+            int result;
+
+            foreach (string value in values)
+            {
+                try
+                {
+                    result = Convert.ToInt32(value);
+                    Console.WriteLine("Converted the {0} value '{1}' to the {2} value {3}.",
+                                      value.GetType().Name, value, result.GetType().Name, result);
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("{0} is outside the range of the Int32 type.", value);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("The {0} value '{1}' is not in a recognizable format.",
+                                      value.GetType().Name, value);
+                }
+            }
+        }
+    }
+}
